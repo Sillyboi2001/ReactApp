45d8d1cc8761a202e83b699347ebd1c54d4bfc0f
Sillyboi2001 <65086841+Sillyboi2001@users.noreply.github.com>
2022-08-04 12:39:41 +0100

    Revert "Book creation implementation"

--- e-library/src/Components/forms/createbook.jsx
+++ e-library/src/Components/forms/createbook.jsx
@@ -1,69 +1,29 @@
-import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import axios from "../api/axios";
-import formData from 'form-data'
-import { token } from './input';
+import React from "react";
 
-const CreateBook = () => {;
-    const [title, setTitle] = useState('');
-    const [price, setPrice] = useState('')
-    const [author, setAuthor] = useState('');
-    const [fileUrl, setFileUrl] = useState(null)
-    const [description, setDescription] = useState('')
-
-    const redirect = useNavigate()
-
-    const submit = async (e) => {
-        e.preventDefault()
-        const form = new formData()
-        form.append('title', title)
-        form.append('price', price)
-        form.append('author', author)
-        form.append('description', description)
-        form.append('bookFile', fileUrl)
-        try {
-            const response = await axios.post('/api/books', form, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'authorization': token.id
-                }
-            });
-            setTitle('');
-            setPrice('');
-            setFileUrl('');
-            setAuthor('');
-            setDescription('');
-            window.alert('Book has been created successfully')
-            redirect('/homepage')
-            console.log(response.data)
-        } catch (err){
-              if (err) {
-                  window.alert('Failed to create book. Please try again')
-                  redirect('/homepage')
-            }
-        }
-    }
-        return(
-            <>      
-               <div> 
-                    <form onSubmit={submit}>
-                        <h2>Create your book</h2><div className="input-box">
-                            <input type="text" value={title} placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
-                        </div><div className="input-box">
-                            <textarea placeholder="Description" value={description} rows="4" columns="50" onChange={(e) => setDescription(e.target.value)}></textarea>
-                        </div><div className="input-box">
-                            <input type="text" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-                        </div><div className="input-box">
-                            <input type="text" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
-                        </div><div className="input-box">
-                            <input type="file" onChange={(e) => setFileUrl(e.target.files[0])} />
-                        </div><div className="input-box">
-                            <input type="submit" value="Create your book" />
-                        </div>
-                    </form>
-                </div>
-            </>
-        )    
+const CreateBook = () => {
+    return(
+        <form >
+        <h2>Create your book</h2>
+        <div className="input-box">
+            <input type="text" placeholder="Title"/>
+        </div>
+        <div className="input-box">
+           <textarea placeholder="Description" rows="4" columns="50"></textarea>
+        </div>
+        <div className="input-box">
+            <input type="text" placeholder="Author"/>
+        </div>
+        <div className="input-box">
+            <input type="text" placeholder="Price"/>
+        </div>
+        <div className="input-box">
+            <input type="file"/>
+        </div>
+        <div className="input-box">
+            <input type="submit" value="Create your book"/>
+        </div>
+    </form>
+    )
 }
 
 export default CreateBook
